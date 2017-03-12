453828c6db23cd3760a0212fc1933a6fb6b85850
Sky Bohannon <skybohannon@gmail.com>
2017-03-12 18:22:56 -0500

    Working from new file

--- urkel.py
+++ urkel.py
@@ -10,66 +10,62 @@ urkelbot
   \____\____/\\_____\____/___\  ______//___/___/___/___/\\_____/___/___/
                               \/
 """
-
+from __future__ import print_function
 import plugins
 import requests
 import re
 import subprocess
-import urllib.parse
-from bs4 import BeautifulSoup
-from datetime import date
 import json
 import io
 import os
 import aiohttp
+import csv
+import urllib.parse
+from bs4 import BeautifulSoup
+
 
 def _initialize(bot):
-    plugins.register_user_command(["fpl"])
-    plugins.register_user_command(["players"])
     plugins.register_user_command(["uptime"])
-
-def fpl(bot, event, *args):
-    if "ping" in args:
-        ping_stuff = ping()
-        yield from bot.coro_send_message(event.conv_id, ping_stuff)
-    else:
-        server_stuff = get_status()
-        yield from bot.coro_send_message(event.conv_id, server_stuff)
-
-
-def players(bot, event, *args):
-    players = get_status(players=True)
-    yield from bot.coro_send_message(event.conv_id, players)
-
+    plugins.register_user_command(["fpl"])
+    plugins.register_user_command(["ping"])
 
 def uptime(bot, event, *args):
-    proc = subprocess.check_output(['uptime']).decode('utf-8')
-    time = re.search(r'up (.*?),', proc)
+    proc1 = subprocess.check_output(['uptime']).decode('utf-8').strip("\n")
+    time = re.search(r'up (.* days)?', proc1)
     proc2 = subprocess.check_output(['uptime', '-p']).decode('utf-8').strip("\n")
     time_pretty = re.search(r'up (.*)?', proc2)
     time_strings = [time_pretty.group(1), "(" + time.group(1) + ")"]
     time_stuff = " ".join(time_strings)
     yield from bot.coro_send_message(event.conv_id, time_stuff)
 
-
-def get_status(players=False):
-    url = 'https://fantasy.premierleague.com/a/leagues/standings/229216/h2h/'
+def fpl(bot, event, *args):
+    url = 'http://www.fplstatistics.co.uk/Home/IndexWG'
     header = {'User-Agent': 'Mozilla/5.0'}
+
     r = urllib.request.Request(url=url, headers=header)
     page = urllib.request.urlopen(r)
     soup = BeautifulSoup(page.read(), "lxml")
-    string = '.ism-main h2.ism-section-title, .ism-table--standings tr:nth-of-type(1) td:nth-of-type(1), .ism-table--standings tr:nth-of-type(1) td:nth-of-type(1) strong'
+    string = 'tbody tr:nth-of-type(1) td:nth-of-type(1), tbody tr:nth-of-type(1) td:nth-of-type(2), tbody tr:nth-of-type(1) td:nth-of-type(6), tbody tr:nth-of-type(1) td:nth-of-type(9) div'
     data = soup.select(string)
 
-    title = data[1].contents[0].strip()
-    rank = data[2].contents[0].strip()
-    teamName = data[3].contents[0].strip()
-    return title
-    return test
-    return teamName
+    playerName = data[0].contents[0].strip()
+    playerTeam = data[1].contents[0].strip()
+    playerPrice = data[2].contents[0].strip()
+    playerTarget = data[3].contents[0].strip()
 
+    fpl_strings = [playerName, playerTeam, playerPrice, playerTarget] 
+    print(" ".join(fpl_strings))
+    return "<br/>".join(fpl_strings)
 
-def ping():
+    yield from bot.coro_send_message(event.conv_id, fpl_strings)
+
+def ping(bot, event, *args):
     server = "209.95.56.13"
-    proc = subprocess.check_output(['ping', '-c1', server])
-    return ping == 0
+    try:
+        proc = subprocess.check_output(['ping', '-c1', server])
+        string = proc.decode('utf-8')
+        ping = re.search(r'rtt min/avg/max/mdev = \d+\.\d+/(\d+\.\d+)/\d+\.\d+/', string)
+        ping_stuff = ping.group(1)
+        yield from bot.coro_send_message(event.conv.id_, server + ": " + ping_stuff)
+    except subprocess.CalledProcessError:
+        yield from bot.coro_send_message(event.conv.id_, "Ping command doesn't work right now")
