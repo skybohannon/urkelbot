53b1fef29ab5fcea14e884b5fc1a1d5602868752
Sky Bohannon <skybohannon@gmail.com>
2018-01-06 23:12:38 -0600

    Fix to add error message
    Changed redundant .upper()

--- crypto-symbol.py
+++ crypto-symbol.py
@@ -8,21 +8,23 @@ encoding = webURL.info().get_content_charset('utf-8')
 crypto_list = json.loads(data.decode(encoding))
 
 def symbol(sym):
-
+    sym = sym.upper()
     for coin in crypto_list:
 
-        if coin["symbol"] == sym.upper():
+        if coin["symbol"] == sym:
             coin_name = coin["name"]
             usd_price = float(coin["price_usd"])
             btc_price = float(coin["price_btc"])
             change_24h = float(coin["percent_change_24h"])
             change_1h = float(coin["percent_change_1h"])
 
-    symbol_output = "<b>" + coin_name + " (" + sym.upper() + ")</b>\n\n<b>USD</b>: ${:,.3f}\n"\
-        "<b>BTC</b>: {:.7f}\n"\
-        "<b>24H Change</b>: {:+.2f}%\n"\
-        "<b>1H Change</b>: {:+.2f}%"\
-        .format(usd_price, btc_price, change_1h, change_24h)
+            symbol_output = "<b>" + coin_name + " (" + sym + ")</b>\n\n<b>USD</b>: ${:,.3f}\n" \
+                            "<b>BTC</b>: {:.7f}\n" \
+                            "<b>24H Change</b>: {:+.2f}%\n" \
+                            "<b>1H Change</b>: {:+.2f}%" \
+                            .format(usd_price, btc_price, change_1h, change_24h)
+        else:
+            symbol_output = "There was no match for <b>\"{}\"</b>".format(sym)
 
     return symbol_output
 
