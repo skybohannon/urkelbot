2e4e90bb476f7ba50ad0098c66014ad660697b98
Sky Bohannon <skybohannon@gmail.com>
2018-01-31 21:06:46 -0600

    start work on getting lastfm-np to post album art

--- lastfm-np.py
+++ lastfm-np.py
@@ -1,6 +1,10 @@
 import urllib.request
 import json
-
+import os
+import aiohttp
+import io
+import asyncio
+import re
 
 def np(user):
     user = user.lower()
@@ -25,13 +29,17 @@ def np(user):
     data = webURL.read()
     encoding = webURL.info().get_content_charset('utf-8')
     recent_tracks = json.loads(data.decode(encoding))
-
+    recent_tracks.update(recent_tracks["recenttracks"])
     tracks_dict = {}
     counter = 1
+
+    print(recent_tracks)
+
     for track in recent_tracks:
         tracks_dict[counter] = {
-            "artist": recent_tracks["recenttracks"]["track"][0]["artist"]["#text"],
-            "title": recent_tracks["recenttracks"]["track"][0]["name"],
+            "artist": recent_tracks["track"][0]["artist"]["#text"],
+            "title": recent_tracks["track"][0]["name"],
+            "album_art": recent_tracks["track"][0]["image"][0]["#text"]
         }
         try:
             if recent_tracks["recenttracks"]["track"][0]["@attr"]["nowplaying"] == "true":
@@ -44,6 +52,19 @@ def np(user):
     else:
         now_playing = "<b>{}'s Last Played Track</b>:\n".format(user.capitalize()) + tracks_dict[1]["artist"] + " - " + tracks_dict[1]["title"]
 
+    print(tracks_dict[1]["album_art"])
+
+
+    image = tracks_dict[1]["album_art"]
+    imagelink = tracks_dict[1]["album_art"]
+    filename = os.path.basename(imagelink)
+    r = yield from aiohttp.request('get',imagelink)
+    raw = yield from r.read()
+    image_data = io.BytesIO(raw)
+    image_id = yield from bot._client.upload_image(image_data, filename=filename)
+    yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+
+    
     return now_playing
 
 print(np("sky"))
