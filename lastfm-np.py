b7844983da03f109df33b55124e7b5e7e8a62f2c
Sky Bohannon <skybohannon@gmail.com>
2018-02-01 20:41:55 -0600

    Last.fm update - Show album art
    Got rid of unneeded dictionary as well
    Fixed some errors

--- lastfm-np.py
+++ lastfm-np.py
@@ -20,51 +20,48 @@ def np(user):
     try:
         with open("lastfm-" + user + "-api.txt", "r") as api_key:
             api_key = api_key.read()
+
+        urlData = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username + "&api_key=" + api_key + "&format=json"
+
+        webURL = urllib.request.urlopen(urlData)
+        data = webURL.read()
+        encoding = webURL.info().get_content_charset('utf-8')
+        recent_tracks = json.loads(data.decode(encoding))
+        recent_tracks.update(recent_tracks["recenttracks"])
+
+        track_artist = recent_tracks["track"][0]["artist"]["#text"]
+        track_title = recent_tracks["track"][0]["name"]
+        track_art = recent_tracks["track"][0]["image"][3:4]
+        track_art = track_art[0]["#text"]
+        now_playing = "false"
+
+        try:
+            if recent_tracks["track"][0]["@attr"]["nowplaying"] == "true":
+                now_playing = "true"
+        except KeyError:
+            now_playing = "false"
+
+        if now_playing == "true":
+            now_playing = "<b>{}'s Now Playing</b>:\n".format(user.capitalize()) + track_artist + " - " + track_title
+        else:
+            now_playing = "<b>{}'s Last Played Track</b>:\n".format(
+                user.capitalize()) + track_artist + " - " + track_title
+
+        # try:
+        #     image = track_art
+        #     imagelink = track_art
+        #     filename = os.path.basename(imagelink)
+        #     r = yield from aiohttp.request('get', imagelink)
+        #     raw = yield from r.read()
+        #     image_data = io.BytesIO(raw)
+        #     image_id = yield from bot._client.upload_image(image_data, filename=filename)
+        #     yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+        # except ValueError:
+        #     pass
+
     except FileNotFoundError:
         return "Could not find user {}".format(user)
 
-    urlData = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username + "&api_key=" + api_key +"&format=json"
-
-    webURL = urllib.request.urlopen(urlData)
-    data = webURL.read()
-    encoding = webURL.info().get_content_charset('utf-8')
-    recent_tracks = json.loads(data.decode(encoding))
-    recent_tracks.update(recent_tracks["recenttracks"])
-    tracks_dict = {}
-    counter = 1
-
-    print(recent_tracks)
-
-    for track in recent_tracks:
-        tracks_dict[counter] = {
-            "artist": recent_tracks["track"][0]["artist"]["#text"],
-            "title": recent_tracks["track"][0]["name"],
-            "album_art": recent_tracks["track"][0]["image"][0]["#text"]
-        }
-        try:
-            if recent_tracks["recenttracks"]["track"][0]["@attr"]["nowplaying"] == "true":
-                tracks_dict[counter].update({"nowplaying": "true"})
-        except KeyError:
-            tracks_dict[counter].update({"nowplaying": "false"})
-
-    if tracks_dict[1]["nowplaying"] == "true":
-        now_playing = "<b>{}'s Now Playing</b>:\n".format(user.capitalize()) + tracks_dict[1]["artist"] + " - " + tracks_dict[1]["title"]
-    else:
-        now_playing = "<b>{}'s Last Played Track</b>:\n".format(user.capitalize()) + tracks_dict[1]["artist"] + " - " + tracks_dict[1]["title"]
-
-    print(tracks_dict[1]["album_art"])
-
-
-    image = tracks_dict[1]["album_art"]
-    imagelink = tracks_dict[1]["album_art"]
-    filename = os.path.basename(imagelink)
-    r = yield from aiohttp.request('get',imagelink)
-    raw = yield from r.read()
-    image_data = io.BytesIO(raw)
-    image_id = yield from bot._client.upload_image(image_data, filename=filename)
-    yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
-
-    
     return now_playing
 
 print(np("sky"))
--- urkel.py
+++ urkel.py
@@ -22,6 +22,10 @@ import urllib.request
 import json
 import os
 import pytz
+import aiohttp
+import asyncio
+import io
+
 file_path = "/home/sky/hangoutsbot/hangupsbot/plugins/"
 
 def _initialize(bot):
@@ -461,24 +465,36 @@ def np(bot, event, user):
         data = webURL.read()
         encoding = webURL.info().get_content_charset('utf-8')
         recent_tracks = json.loads(data.decode(encoding))
+        recent_tracks.update(recent_tracks["recenttracks"])
 
-        tracks_dict = {}
-        counter = 1
-        for track in recent_tracks:
-            tracks_dict[counter] = {
-                "artist": recent_tracks["recenttracks"]["track"][0]["artist"]["#text"],
-                "title": recent_tracks["recenttracks"]["track"][0]["name"],
-            }
-            try:
-                if recent_tracks["recenttracks"]["track"][0]["@attr"]["nowplaying"] == "true":
-                    tracks_dict[counter].update({"nowplaying": "true"})
-            except KeyError:
-                tracks_dict[counter].update({"nowplaying": "false"})
+        track_artist = recent_tracks["track"][0]["artist"]["#text"]
+        track_title = recent_tracks["track"][0]["name"]
+        track_art = recent_tracks["track"][0]["image"][3:4]
+        track_art = track_art[0]["#text"]
+        now_playing = "false"
 
-        if tracks_dict[1]["nowplaying"] == "true":
-            now_playing = "<b>{}'s Now Playing</b>:\n".format(user.capitalize()) + tracks_dict[1]["artist"] + " - " + tracks_dict[1]["title"]
+        try:
+            if recent_tracks["track"][0]["@attr"]["nowplaying"] == "true":
+                now_playing = "true"
+        except KeyError:
+            now_playing = "false"
+
+        if now_playing == "true":
+            now_playing = "<b>{}'s Now Playing</b>:\n".format(user.capitalize()) + track_artist + " - " + track_title
         else:
-            now_playing = "<b>{}'s Last Played Track</b>:\n".format(user.capitalize()) + tracks_dict[1]["artist"] + " - " + tracks_dict[1]["title"]
+            now_playing = "<b>{}'s Last Played Track</b>:\n".format(
+                user.capitalize()) + track_artist + " - " + track_title
+        try:
+            image = track_art
+            imagelink = track_art
+            filename = os.path.basename(imagelink)
+            r = yield from aiohttp.request('get', imagelink)
+            raw = yield from r.read()
+            image_data = io.BytesIO(raw)
+            image_id = yield from bot._client.upload_image(image_data, filename=filename)
+            yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+        except ValueError:
+            pass
 
     except FileNotFoundError:
         now_playing = "Could not find user {}".format(user)
